ee695eee69c72df7bf9b07a40fb4e961c67ab57f
ManoharPulluru <125332698+ManoharPulluru@users.noreply.github.com>
2023-06-14 18:49:15 +0530

    Final Modfications1

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
+const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt');
 const userSignup = require('./userData/singup');
 const userLogin = require('./userData/login');
@@ -20,8 +21,9 @@ const findUserByContact = require('./userData/findUserByContact');
 const globalModel = require('./model/globalModel');
 const getglobaldata = require('./userData/getGlobalData');
 const getGlobalDataByEvNameNContact = require('./userData/getGlobalDataByEvNameNContact');
-app.use(express.json());
 app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.listen(3300, () => {
     console.log('Running on port 3300');
 })
